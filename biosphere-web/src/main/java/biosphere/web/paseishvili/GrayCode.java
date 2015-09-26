075e16ce925ceac715a1632cc9b46b010e35f4e5
Alexey.Paseishvili <apaseishvili@gmail.com>
2015-09-26 08:40:59 +0300

    Paseishvili init commit

---
+++ biosphere-web/src/main/java/biosphere/web/paseishvili/GrayCode.java
@@ -0,0 +1,7 @@
+package biosphere.web.paseishvili;
+
+/**
+ * Created by UNBREAKING on 25.09.2015.
+ */
+public class GrayCode {
+}
---
+++ biosphere-web/src/main/java/biosphere/web/paseishvili/GrayCodeTester.java
@@ -0,0 +1,26 @@
+package biosphere.web.paseishvili;
+
+/**
+ * Created by UNBREAKING on 25.09.2015.
+ */
+import java.util.Scanner;
+
+public class GrayCodeTester {
+    public static void main(String [] args){
+        int P=1;
+        int k;
+        Scanner in=new Scanner(System.in);
+        System.out.println("this is test factorial program");
+        System.out.println("Write down the number");
+        k= in.nextInt();
+        if(k>0){
+        for(int i=1;i<=k;i++)
+            P=P*i;
+            System.out.println("factorial of your number is:");
+            System.out.println(P);
+        }else{
+            System.out.println("ERROR");
+        }
+
+    }
+}
