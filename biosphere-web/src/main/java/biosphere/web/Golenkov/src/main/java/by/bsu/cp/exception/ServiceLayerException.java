ed86272d53e1db54e5e1dd92b23299b34f1581ff
Aleksey.Golenkov <asgolenkov9797@gmail.com>
2016-05-26 00:13:36 +0300

    Golenkov init commit

---
+++ biosphere-web/src/main/java/biosphere/web/Golenkov/src/main/java/by/bsu/cp/exception/ServiceLayerException.java
@@ -0,0 +1,14 @@
+package by.bsu.cp.exception;
+
+/**
+ * Created by Alexey on 5/25/2016.
+ */
+public class ServiceLayerException extends Exception {
+    public ServiceLayerException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceLayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
