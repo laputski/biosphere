3c3ab12fee22f556e5524376b90b206fe54074af
Alexey.Burkovski <29119555@mail.ru>
2016-05-28 00:00:33 +0300

    Burkovski  init  commit
    Burkovski  init  commit

---
+++ biosphere-web/src/main/java/biosphere/web/ABurkovski/lb4/src/main/java/by/bsu/cp/exception/DaoLayerException.java
@@ -0,0 +1,10 @@
+package biosphere.web.filippovich.lb4.src.main.java.by.bsu.cp.exception;
+
+/**
+ * Created by Varvara on 5/27/2016.
+ */
+public class DaoLayerException extends Exception {
+    public DaoLayerException(Throwable cause) {
+        super(cause);
+    }
+}
