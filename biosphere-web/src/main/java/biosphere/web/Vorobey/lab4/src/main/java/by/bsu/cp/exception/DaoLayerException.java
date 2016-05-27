730a8900683ab21b8d8a673d088b0ba9133ea9eb
Vladislav.Vorobey <vladislav.mmf@gmail.com>
2016-05-27 22:19:20 +0300

    Vorobey lb 4 init commit

---
+++ biosphere-web/src/main/java/biosphere/web/Vorobey/lab4/src/main/java/by/bsu/cp/exception/DaoLayerException.java
@@ -0,0 +1,10 @@
+package by.bsu.cp.exception;
+
+/**
+ * Created by Vlad on 5/27/2016.
+ */
+public class DaoLayerException extends Exception {
+    public DaoLayerException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ biosphere-web/src/main/java/biosphere/web/Vorobey/lab4/src/main/java/by/bsu/cp/exception/ServiceLayerException.java
@@ -0,0 +1,14 @@
+package by.bsu.cp.exception;
+
+/**
+ * Created by Vlad on 5/27/2016.
+ */
+public class ServiceLayerException extends Exception {
+    public ServiceLayerException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceLayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
