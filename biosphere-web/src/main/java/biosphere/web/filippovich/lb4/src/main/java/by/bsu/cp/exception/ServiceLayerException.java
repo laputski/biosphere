e4d9d4bb9401c21de8c67af1f7beb9803e6b855a
Pavel.Filippovich <jugorbd@gmail.com>
2016-05-27 23:05:50 +0300

    Filippovich init commit

---
+++ biosphere-web/src/main/java/biosphere/web/filippovich/lb4/src/main/java/by/bsu/cp/exception/ServiceLayerException.java
@@ -0,0 +1,14 @@
+package biosphere.web.filippovich.lb4.src.main.java.by.bsu.cp.exception;
+
+/**
+ * Created by Pavel on 5/27/2016.
+ */
+public class ServiceLayerException extends Exception {
+    public ServiceLayerException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceLayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
