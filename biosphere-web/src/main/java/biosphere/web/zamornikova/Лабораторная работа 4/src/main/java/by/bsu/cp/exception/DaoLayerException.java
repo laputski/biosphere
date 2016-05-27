0cec62ed90105e8c61fc5632f51a74894990f16d
Natalia.Zamornikova <natasik1313@gmail.com>
2016-05-28 01:37:23 +0300

    Zamornikova init commit

---
+++ biosphere-web/src/main/java/biosphere/web/zamornikova/Лабораторная работа 4/src/main/java/by/bsu/cp/exception/DaoLayerException.java
@@ -0,0 +1,10 @@
+package by.bsu.cp.exception;
+
+/**
+ * Created by Natalia on 5/27/2016.
+ */
+public class DaoLayerException extends Exception {
+    public DaoLayerException(Throwable cause) {
+        super(cause);
+    }
+}
